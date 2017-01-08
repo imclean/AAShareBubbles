6732766ecd363fac2eef6859558ac176c57629e3
Iain McLean <iain@brightskyapps.com>
2017-01-08 22:02:49 +0000

    can’t be 0 alpha or acts as hidden

--- AAShareBubbles/AAShareBubbles.m
+++ AAShareBubbles/AAShareBubbles.m
@@ -44,7 +44,7 @@
         self.radius = radiusValue;
         self.bubbleRadius = 40;
         self.parentView = inView;
-        self.faderAlpha = 0.0;
+        self.faderAlpha = 0.01;
         self.faderColor = [UIColor blackColor];
         
         self.facebookBackgroundColorRGB = 0x3c5a9a;
