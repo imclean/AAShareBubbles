85772e3d045782a01c2bc555fefb41390b8b78f5
Iain McLean <iain@brightskyapps.com>
2017-01-08 21:18:40 +0000

    removed background

--- AAShareBubbles/AAShareBubbles.m
+++ AAShareBubbles/AAShareBubbles.m
@@ -44,7 +44,7 @@
         self.radius = radiusValue;
         self.bubbleRadius = 40;
         self.parentView = inView;
-        self.faderAlpha = 0.6;
+        self.faderAlpha = 0.0;
         self.faderColor = [UIColor blackColor];
         
         self.facebookBackgroundColorRGB = 0x3c5a9a;
